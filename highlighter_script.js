000b6923a2175b447cee7ef02e9e6958b9993f29
Shivang Bansal <shivangbansal1995@outlook.com>
2020-07-04 15:07:43 +0530

    add highlighting

--- highlighter_script.js
+++ highlighter_script.js
@@ -1,13 +1,10 @@
 
- setTimeout(function() {
- 
- 	for(var i = 0; i < questions.length; i++){
-  		var x = document.querySelectorAll("td[value='"+questions[i]+"']");
+setTimeout(function() {
+	for(var i = 0; i < questions.length; i++){
+		var x = document.querySelectorAll("td[value='"+questions[i]+"']");
 		if (x.length > 0){
-  			flag = false;
-  	  		x[0].innerHTML =x[0].innerHTML+ " 👍"; 
-  		}
- 	}
-  
-
- }, 5000);
+			flag = false;
+			x[0].innerHTML = x[0].innerHTML.replace(questions[i], "<mark><b>"+questions[i]+"</b></mark>"); 
+		}
+	}
+}, 10000);
